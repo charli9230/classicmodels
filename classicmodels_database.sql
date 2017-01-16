09d9585d4808fe67b0546b02f96b60d375db8b05
Lebon Charles <s208059956@nmmu.ac.za>
2017-01-16 16:44:14 +0200

    Update classicmodels_database.sql

--- classicmodels_database.sql
+++ classicmodels_database.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jan 11, 2017 at 09:14 PM
--- Server version: 5.7.9
--- PHP Version: 5.6.16
+CREATE DATABASE IF NOT EXISTS classicmodels;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `classicmodels`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `productline`
---
-
+USE classicmodels;
 DROP TABLE IF EXISTS `productline`;
 CREATE TABLE IF NOT EXISTS `productline` (
   `productLine` varchar(50) NOT NULL,
@@ -33,20 +8,12 @@ CREATE TABLE IF NOT EXISTS `productline` (
   PRIMARY KEY (`productLine`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `productline`
---
-
+USE classicmodels;
 INSERT INTO `productline` (`productLine`, `textDescription`) VALUES
 ('HY76', 'DVD PLAYER'),
 ('PORT', 'DSTV DECODER');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
-
+USE classicmodels;
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `productCode` varchar(15) NOT NULL,
@@ -62,24 +29,16 @@ CREATE TABLE IF NOT EXISTS `products` (
   KEY `products_productline_fk_idx` (`productLine`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `products`
---
-
+USE classicmodels;
 INSERT INTO `products` (`productCode`, `productName`, `productLine`, `productScale`, `productVendor`, `productDescription`, `quantityInStock`, `buyPrice`, `MSRP`) VALUES
 ('PC101', 'TABLE', 'HY76', '1:89', 'WOOD', 'HOME FURNITURE', 56, '2589.00', '2500.00'),
 ('PC121', 'LAPTOP', 'HY76', '1:50', 'JVC', 'PERSONAL COMPUTER', 12, '4999.99', '4800.00');
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `products`
---
+USE classicmodels;
 ALTER TABLE `products`
   ADD CONSTRAINT `products_productline_fk` FOREIGN KEY (`productLine`) REFERENCES `productline` (`productLine`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+USE classicmodels;
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON * . * TO 'user'@'localhost';
+
